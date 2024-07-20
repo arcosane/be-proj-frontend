cd81f9345b9c0eb262b2d4cbc5e1668bd6bcb652
arcosane <castelinoaston123@gmail.com>
2024-07-20 20:08:03 +0530

    login and home page

--- app/home/page.js
+++ app/home/page.js
@@ -1,9 +1,24 @@
-import React from 'react'
+'use client';
+
+import { useSearchParams } from 'next/navigation';
+import { useEffect, useState } from 'react';
 
 export default function page() {
+  const searchParams = useSearchParams();
+  const [username, setUsername] = useState('');
+
+  useEffect(() => {
+    const usernameParam = searchParams.get('username');
+    if (usernameParam) {
+      setUsername(usernameParam);
+    }
+  }, [searchParams]);
+
   return (
-    <div>
-      HOME PAGE
+    <div className="flex items-center justify-center min-h-screen bg-gray-100">
+      <div className="p-6 bg-white rounded shadow-md">
+        <h1 className="mb-4 text-2xl font-bold">Welcome, {username}!</h1>
+      </div>
     </div>
-  )
+  );
 }
--- app/login/page.js
+++ app/login/page.js
@@ -1,9 +1,35 @@
-import React from 'react'
+"use client"
+import { useRouter } from 'next/navigation';
 
 export default function page() {
+  const router = useRouter();
+
+  const handleGitHubLogin = async () => {
+    try {
+      const response = await fetch('http://127.0.0.1:8000/api/github-login/', {
+        method: 'GET',
+        credentials: 'include',
+      });
+      const data = await response.json();
+      if (data.authorization_url) {
+        window.location.href = data.authorization_url;
+      }
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  };
+
   return (
-    <div>
-      LOGIN PAGE
+    <div className="flex items-center justify-center min-h-screen bg-gray-100">
+      <div className="p-6 bg-white rounded shadow-md">
+        <h1 className="mb-4 text-2xl font-bold text-center">Login</h1>
+        <button
+          onClick={handleGitHubLogin}
+          className="w-full px-4 py-2 text-white bg-gray-800 rounded hover:bg-gray-700"
+        >
+          Login with GitHub
+        </button>
+      </div>
     </div>
-  )
+  );
 }
